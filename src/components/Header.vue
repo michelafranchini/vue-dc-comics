377453886e8874aea157ad9e5bc428aabfff2b52
michelafranchini <michela.franchini.91@gmail.com>
2021-06-07 17:37:20 +0200

    Ciclo for su li navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,10 @@
         <div class="container">
             <img src="../assets/img/dc-logo.png" alt="Logo DC">
             <ul>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Comics</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">Games</a></li>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :href="link.link"> {{link.text}}</a>
+                </li>
+                
             </ul>
         </div>
     </header>
@@ -18,6 +18,62 @@
 
 export default {
   name: 'Header',
+  data () {
+      return {
+          links: [
+            {
+              text: "characters", 
+              link: "link", 
+              active: false
+            }, 
+            {
+              text: "comics", 
+              link: "link", 
+              active: true
+            }, 
+            {
+              text: "movies", 
+              link: "link", 
+              active: false
+            }, 
+            {
+              text: "tv", 
+              link: "link", 
+              active: false
+            }, 
+            {
+              text: "games", 
+              link: "link", 
+              active: false
+            }, 
+            {
+              text: "collectibles", 
+              link: "link", 
+              active: false
+            }, 
+            {
+              text: "videos", 
+              link: "link", 
+              active: false
+            },
+            {
+              text: "fans", 
+              link: "link", 
+              active: false
+            },
+            {
+              text: "news", 
+              link: "link", 
+              active: false
+            },
+            {
+              text: "shop", 
+              link: "link", 
+              active: false
+            },
+          ]
+        }
+    }
 }
 </script>
 
@@ -26,7 +82,6 @@ export default {
         display: flex;
         align-items: center;
         height: 120px;
-        background-color: lightblue;
     }
     .container {
         width: 70%;
@@ -47,6 +102,10 @@ export default {
                 text-decoration: none;
                 text-transform: uppercase;
                 color: #1c1c1c;
+            }
+
+            a:hover {
+                color: #0282f9; 
             }
 
         }
